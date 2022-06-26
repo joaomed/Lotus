410ec5c8ff0365db5f6aa479fa7d7800a1a5088b
João Vitor Medeiros <joaoolimedeiros@gmail.com>
2022-06-25 22:53:55 -0300

    init startCountdown function

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,22 +1,51 @@
+import { useEffect, useState } from 'react'
 import styles from '../styles/components/Countdown.module.css'
 
 export function Countdown(){ 
+  const [time, setTime] = useState(25 * 60)
+  const [active,setActive] = useState(false)
+
+  const minutes = Math.floor(time/60)
+  const seconds = time % 60
+
+  const [minuteLeft, minuteRight] = String(minutes).padStart(2,'0').split('')
+  const [secondLeft, secondRight] = String(seconds).padStart(2,'0').split('')
+
+  function startCountdown(){
+    setActive(true)
+  }
+
+  // useEffect = quero que execute uma função quando o "active" muda e o "time"
+  useEffect(()=>{
+    if(active && time>0){
+      // setTimeout= quero que aconteça algo depois de um tempo
+      // vai executar uma função depois de 1segundo
+      setTimeout(()=>{
+        setTime(time - 1)
+      },1000)
+    }
+  },[active, time])
+
   return(
     <div>
       <div className={styles.countdownContainer}>
         <div>
-          <span>2</span>
-          <span>2</span>     
+          <span>{minuteLeft}</span>
+          <span>{minuteRight}</span>     
         </div>
 
         <span>:</span>
 
         <div>
-          <span>0</span>
-          <span>0</span>     
+          <span>{secondLeft}</span>
+          <span>{secondRight}</span>     
         </div>
       </div>
-      <button type='button' className={styles.countdownButton}> Iniciar um ciclo</button>
+      <button 
+      type='button' 
+      className={styles.countdownButton}
+      onClick={startCountdown}
+      > Iniciar um ciclo</button>
     </div>
   )
 }
