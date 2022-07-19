4e7d6770f3a812e867b38e4b45def0cd42ab39ab
João Vitor Medeiros <joaoolimedeiros@gmail.com>
2022-07-19 18:56:06 -0300

    upd ChallengeBox Component
    Implement reset countdown in ChallengeSucceed and ChallengeFailed

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -1,12 +1,24 @@
+import { useContext } from 'react';
 import { ChallengesContext } from '../contexts/ChallengesContext';
+import { CountdownContext } from '../contexts/CountdownContext';
 
 import styles from '../styles/components/ChallengeBox.module.css'
 import { ImArrowUp} from 'react-icons/im'
 import { BiWalk} from 'react-icons/bi'
-import { useContext } from 'react';
 
 export function ChallengeBox(){
-  const { activeChallenge, resetChallenge } = useContext(ChallengesContext);  
+  const { activeChallenge, resetChallenge, completeChallenge } = useContext(ChallengesContext);   
+  const { resetCountdown } = useContext(CountdownContext); 
+  
+  function handleChallengeSucceeded(){
+    completeChallenge();
+    resetCountdown();
+  }
+
+  function handleChallengeFailed(){
+    resetChallenge();
+    resetCountdown();    
+  }
 
   return(
 
@@ -24,13 +36,14 @@ export function ChallengeBox(){
             <button 
             type='button' 
             className={styles.challengeFailedButton}
-            onClick={resetChallenge}
+            onClick={handleChallengeFailed}
             >Falhei
             
             </button>
             <button 
             type='button' 
             className={styles.challengeSucceededButton}
+            onClick={handleChallengeSucceeded}
             >Completei</button>
           </footer>
         </div>
