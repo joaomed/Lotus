59e9bd57c9e49bee9f15e5f5ead71c97d3a3c9d7
João Vitor Medeiros <joaoolimedeiros@gmail.com>
2022-07-19 16:03:21 -0300

    upd ChallengeBox Component

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -1,28 +1,30 @@
+import { ChallengesContext } from '../contexts/ChallengesContext';
+
 import styles from '../styles/components/ChallengeBox.module.css'
 import { ImArrowUp} from 'react-icons/im'
 import { BiWalk} from 'react-icons/bi'
-
-
+import { useContext } from 'react';
 
 export function ChallengeBox(){
-  const hasActiveChallenge = true;
+  const { activeChallenge, resetChallenge } = useContext(ChallengesContext);  
 
   return(
 
     <div className={styles.challengeBoxContainer}>
-      {hasActiveChallenge ? (
+      {activeChallenge ? (
         <div className={styles.challengeActive}>
-          <header>Ganhe 400 xp</header>
+          <header>Ganhe {activeChallenge.amount} xp</header>
           <main>          
             <BiWalk  />
             <strong>Novo desafio</strong>
-            <p>Levante e faça uma caminhada de 3 minutos.</p>
+            <p> {activeChallenge.description} </p>
           </main>
 
           <footer>
             <button 
             type='button' 
             className={styles.challengeFailedButton}
+            onClick={resetChallenge}
             >Falhei
             
             </button>
