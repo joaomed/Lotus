e6773ab4a8c311ccaa880142b89a9084760c2510
João Vitor Medeiros <joaoolimedeiros@gmail.com>
2022-07-19 16:04:23 -0300

    upd Profile Component

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,7 +1,12 @@
-import styles from '../styles/components/Profile.module.css'
 import { FaArrowUp} from 'react-icons/fa'
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
+
+import styles from '../styles/components/Profile.module.css'
 
 export function Profile(){
+  const { level } = useContext(ChallengesContext)
+
   return(
     <div className={styles.profileContainer}>
       <img src="https://github.com/joaomed.png" alt="João Vitor Medeiros" />
@@ -9,7 +14,7 @@ export function Profile(){
         <strong>João Vitor Medeiros</strong> 
         <p> 
           <FaArrowUp/>
-          Level 1</p>                
+          Level {level}</p>                
       </div>
     </div>
   );
