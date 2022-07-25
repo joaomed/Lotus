ec180ad8ecca5d0d4d07fc57ddec3fec7cf2dc30
João Vitor Medeiros <joaoolimedeiros@gmail.com>
2022-07-25 17:39:14 -0300

    Update Index Page - GetServerSideProps Method
    Next method for requesting data in cookies, such data will be retrieved inside the intermediate layer with node.js, for search engines to render and use this data.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,9 @@ import '../styles/global.css'
 
 import type { AppProps } from 'next/app'
 
-import { ChallengesProvider } from '../contexts/ChallengesContext'
-
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-
-    <ChallengesProvider>
       <Component {...pageProps} />
-    </ChallengesProvider>
   )
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import type { NextPage } from 'next'
+import { GetServerSideProps } from 'next' 
 
 import Head from 'next/head'
 
@@ -9,36 +9,64 @@ import { Countdown } from '../components/Countdown'
 import { ChallengeBox } from '../components/ChallengeBox'
 
 import styles from '../styles/pages/Home.module.css'
+
 import { CountdownProvider } from '../contexts/CountdownContext'
+import { ChallengesProvider } from '../contexts/ChallengesContext'
 
-const Home: NextPage = () => {
+interface HomeProps {
+  level: number
+  currentExperience: number
+  challengesCompleted: number
+}
+
+export default function Home (props: HomeProps) {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Home | Lotus</title>
-      </Head>
+    <ChallengesProvider 
+      level={props.level}
+      currentExperience={props.currentExperience}
+      challengesCompleted={props.challengesCompleted}    
+    >
 
-      <ExperienceBar/> 
+      <div className={styles.container}>
+        <Head>
+          <title>Home | Lotus</title>
+        </Head>
 
-      <CountdownProvider>
+        <ExperienceBar/> 
 
-        <section>
-          <div>
-            <Profile/>
-            <CompletedChallenges/>
-            <Countdown/>
-          </div> 
+        <CountdownProvider>
 
-          <div>
-            <ChallengeBox/>
-          </div>     
-        </section>
+          <section>
+            <div>
+              <Profile/>
+              <CompletedChallenges/>
+              <Countdown/>
+            </div> 
 
-      </CountdownProvider>
+            <div>
+              <ChallengeBox/>
+            </div>     
+          </section>
 
-    </div>
+        </CountdownProvider>
+
+      </div>
+
+    </ChallengesProvider>
     
   )
 }
 
-export default Home
+// esse metodo recebe um parametro chamado ctx (context)
+export const getServerSideProps: GetServerSideProps = async (ctx)=>{
+
+ const { level, currentExperience, challengesCompleted } = ctx.req.cookies;
+
+ return {
+  props: {
+    level: Number(level),
+    currentExperience: Number(currentExperience),
+    challengesCompleted: Number(challengesCompleted)  
+  }
+ }
+}
